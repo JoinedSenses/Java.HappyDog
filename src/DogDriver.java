c2047c9f3892a2185b487b27b3ada31de562c99d
Arron Vinyard <joinedsenses@gmail.com>
2022-02-10 17:42:00 -0500

    Update DogDriver.java

--- src/DogDriver.java
+++ src/DogDriver.java
@@ -2,74 +2,49 @@ import java.util.Scanner;
 
 public class DogDriver
 {
-
-    public static void main(String[] args)
-    {
-    	boolean gameOver = false;
-        System.out.println("The goal is to make your dog happy.");
-        System.out.println();
+    public static void main(String[] args) {
+        System.out.println("The goal is to make your dog happy.\n");
 
         Scanner keyboard = new Scanner(System.in);
         Dog dog = DogDriver.getDogFromUser(keyboard);
+        
         System.out.println("Commands: play, feed, pet, exit");
-        String line = keyboard.nextLine();
-        while (!line.equals("exit")) //exit means they're done
-        {
-            if (line.equals("play"))
-            {
-                boolean result = dog.play();
-                if (result)
-                {
-                    System.out.println(dog.getName() + " has played with you!");
-                }
-                else
-                {
-                    System.out.println(dog.getName() + " doesn't want to play.");
-                }
-            }
-            else if (line.equals("feed"))
-            {
-                boolean result = dog.feed();
-                if (result)
-                {
-                    System.out.println(dog.getName() + " has eaten.");
-                    gameOver = true;
+        
+        boolean isHappy = false;
+        while (!isHappy) {
+            switch (keyboard.nextLine().trim().toLowerCase()) {
+                case "play":
+                    System.out.println(dog.getName()
+                        + (dog.play() ? " has played with you!" : " doesn't want to play.")
+                    );
                     break;
-                }
-                else
-                {
-                    System.out.println(dog.getName() + " isn't hungry.");
-                }
+                case "feed":
+                    if (dog.feed()) {
+                        System.out.println(dog.getName() + " has eaten.");
+                        isHappy = true;
+                    }
+                    else {
+                        System.out.println(dog.getName() + " isn't hungry.");
+                    }
+                    break;
+                case "pet":
+                    dog.pet();
+                    System.out.println(dog.getName() + " has drooled all over you!");
+                    break;
+                case "exit":
+                    return;
+                default:
+                    System.out.println("Invalid input! Valid commands are: play, feed, pet, exit");
             }
-            else if (line.equals("pet"))
-            {
-                dog.pet();
-                System.out.println(dog.getName() + " has drooled all over you!");
-            }
-            else
-            {
-                System.out.println("Invalid command! Valid commands are: play, feed, pet, exit");
-            }
-            line = keyboard.nextLine();
-        }
-        if (gameOver == true) {
-        	System.out.println("Congrats! " + dog.getName() + " is happy!");
-        }
     }
 
-    public static Dog getDogFromUser(Scanner keyboard)
-    {
-        String name;
-        int age;
-
+    public static Dog getDogFromUser(Scanner keyboard) {
         System.out.print("Enter name: ");
-        name = keyboard.nextLine();
+        String name = keyboard.nextLine();
 
-        System.out.println();
-        System.out.print("Enter age: ");
-        age = Integer.parseInt(keyboard.nextLine());
+        System.out.print("\nEnter age: ");
+        int age = Integer.parseInt(keyboard.nextLine());
 
-        Dog dog = new Dog(name, age, Dog.Status.Status_Idle);
-        return dog;
+        return new Dog(name, age, Dog.Status.Status_Idle);;
     }
 }
